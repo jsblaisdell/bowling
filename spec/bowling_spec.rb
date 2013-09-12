1d4fb46c93b0c657e25c98b0df984cb5aead1d48
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-12 10:18:35 -0500

    DRY code

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -4,24 +4,22 @@ require 'rspec'
 require_relative '../bowling'
 
 describe BowlingGame do
+  let(:game) { BowlingGame.new }
+
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      game = BowlingGame.new
-
       expect(game.rolls).to eq([])
     end
   end
 
   describe '#score' do
     it 'scores the game as 0 when only gutter balls are thrown' do
-      game = BowlingGame.new
       20.times { game.roll(0) }
 
       expect(game.score).to eq(0)
     end
 
     it 'scores the game as 20 when one pin is knocked down each roll' do
-      game = BowlingGame.new
       20.times { game.roll(1) }
 
       expect(game.score).to eq(20)
