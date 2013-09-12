bd3d14483365efb10e4b580ea5a1087446a03dc7
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-12 11:45:06 -0500

    refactor code

--- bowling.rb
+++ bowling.rb
@@ -3,32 +3,81 @@ class BowlingGame
 
   def initialize
     @rolls = []
+    @total_score  = 0
+    @current_roll = 0
   end
 
+  # Record a roll in the game.
+  #
+  # pins - The Integer number of pins knocked down in this roll.
+  #
+  # Returns nothing.
+  def roll(pins)
+    @rolls.push(pins)
+  end
+
+  # Returns the Integer score for this game.
+  #
+  # Returns score.
   def score
-    total_score  = 0
-    current_roll = 0
+    while @current_roll < @rolls.size - 1
+      init_roll
 
-    while current_roll < @rolls.size - 1
-      roll      = @rolls[current_roll]
-      next_roll = @rolls[current_roll + 1]
-
-      if roll == 10
-        total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
-        current_roll +=1
-      elsif roll + next_roll == 10
-        total_score += 10 + @rolls[current_roll + 2]
-        current_roll += 2
+      if strike?
+        score_strike
+      elsif spare?
+        score_spare
       else
-        total_score += roll + next_roll
-        current_roll += 2
+        score_normal
       end
     end
 
-    return total_score
+    return @total_score
   end
 
-  def roll(num)
-    @rolls << num
+  private
+
+  # Initializes score method.
+  def init_roll
+    @roll      = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll + 1]
+  end
+
+  # Returns true for a strike.
+  #
+  # Returns true or false.
+  def strike?
+    @roll == 10
+  end
+
+  # Scores a strike.
+  #
+  # Returns score.
+  def score_strike
+    @total_score += 10 + @rolls[@current_roll + 1] + @rolls[@current_roll + 2]
+    @current_roll += 1
+  end
+
+  # Returns true for a spare.
+  #
+  # Returns true or false.
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+  # Scores a spare.
+  #
+  # Returns score.
+  def score_spare
+    @total_score += 10 + @rolls[@current_roll + 2]
+    @current_roll += 2
+  end
+
+  # Scores normally.
+  #
+  # Returns score.
+  def score_normal
+    @total_score += @roll + @next_roll
+    @current_roll += 2
   end
 end
