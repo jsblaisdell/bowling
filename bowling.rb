35c5c1964a91b0a938b06380ec603641b165ad31
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-12 10:07:40 -0500

    add roll method and tested getting only gutter balls

--- bowling.rb
+++ bowling.rb
@@ -1,6 +1,12 @@
 class BowlingGame
-  attr_accessor :rolls
+  attr_reader :rolls, :score
+
   def initialize
     @rolls = []
+    @score = 0
+  end
+
+  def roll(num)
+    @score += num
   end
 end
